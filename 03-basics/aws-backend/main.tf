8851b227c5f3ba55b1a795d19cb58f96f32146ba
Obaid <obaid@Obaids-MBP.home>
2025-01-26 16:06:52 +0100

    deploy aws-backend

---
+++ 03-basics/aws-backend/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  # Uncomment this code to switch from local backend to remote AWS backend by rerunning `terraform init` after initail `terraform apply`.
+  backend "s3" {
+    bucket         = "og-devops-tf-state-2025"
+    key            = "03-basics/import-bootstrap/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking-2025"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "og-devops-tf-state-2025"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_config" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking-2025"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
---
+++ 03-basics/terraform-cloud-backend/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "remote" {
+    organization = "og-devops"
+
+    workspaces {
+      name = "og-devops-terraform-handson"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
